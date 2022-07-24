6e74bcad698f0083d4272430c9324390eac11970
stutisinha29 <88232935+stutisinha29@users.noreply.github.com>
2022-07-24 21:27:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+
+function setup(){
+canvas= createCanvas(640,480);
+canvas.position(110,250); 
+}
+
+function draw(){
+    circle(90, 200, 60);
+    rect(150, 180, 100, 100, 20);
+    rect(90, 20, 100, 55);
+    ellipse(56, 46, 55, 55);
+}
